ef10058c1e4892af6ee493f33b82a4ea7805d2af
Cashisfsg <cashisfsg@gmail.com>
2024-06-10 22:56:33 +0300

    add content wrapper on video

--- src/pages/main-layout.tsx
+++ src/pages/main-layout.tsx
@@ -5,7 +5,12 @@ import Cats from "@/assets/video/cats.mp4";
 export const MainLayout = () => {
     return (
         <>
-            <video autoPlay muted loop className="background-video">
+            <video
+                autoPlay
+                muted
+                loop
+                className="background-video content-wrapper"
+            >
                 <source src={Cats} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
