1710f61b5a7e78b665b72f7e28a656e1b6cdcd63
Cashisfsg <cashisfsg@gmail.com>
2024-06-20 21:41:30 +0300

    change base url

--- src/shared/api/config.ts
+++ src/shared/api/config.ts
@@ -9,7 +9,7 @@ import { RootStore } from "@/app/providers/redux/types";
 // const initData = tg?.initData;
 
 export const baseQuery = fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_BASE_URL,
+    baseUrl: import.meta.env.VITE_BASE_API_URL,
     prepareHeaders: (headers, { getState }) => {
         const initData = (getState() as RootStore).user.initData;
         if (initData) {
