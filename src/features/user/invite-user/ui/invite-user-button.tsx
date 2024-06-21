0877c5f51a1ad90a7fa8a8f8d00c9355f8535319
Cashisfsg <cashisfsg@gmail.com>
2024-06-21 18:17:30 +0300

    add target blank

--- src/features/user/invite-user/ui/invite-user-button.tsx
+++ src/features/user/invite-user/ui/invite-user-button.tsx
@@ -1,43 +1,31 @@
-import { TelegramClient } from "@/shared/api/types";
-import { composeEventHandlers } from "@/shared/lib/compose-event-handlers";
-import { Button } from "@/shared/ui/button";
-
 interface ShareData {
     url: string;
     message: string;
     to?: string;
 }
 
-interface InviteUserButtonProps extends React.ComponentProps<"button"> {
+interface InviteUserButtonProps extends React.ComponentProps<"a"> {
     shareData: ShareData;
 }
 
 export const InviteUserButton: React.FC<InviteUserButtonProps> = ({
     shareData,
     className = "",
-    onClick,
     children = "Invite a friend",
     ...props
 }) => {
-    const tg = (
-        window as Window & typeof globalThis & { Telegram: TelegramClient }
-    ).Telegram.WebApp;
     const message = encodeURIComponent(shareData?.message || "");
     const params = {
-        url: shareData?.url || "",
+        url: shareData.url,
         text: message,
         to: shareData?.to || "",
     };
     const searchParams = new URLSearchParams(params);
 
-    // const onClickHandler: React.MouseEventHandler<HTMLButtonElement> = () => {
-    //     tg.openLink(`https://t.me/share/url?${searchParams}`);
-    // };
-
     return (
         <a
             href={`https://t.me/share/url?${searchParams}`}
-            // onClick={composeEventHandlers(onClick, onClickHandler)}
+            target="_blank"
             className={`button-base button-blue font-secondary ${className}`}
             {...props}
         >
