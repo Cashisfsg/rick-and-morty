e8ff8f3a1858b45c787aa7ebf8f10a5ce5bcabca
Cashisfsg <cashisfsg@gmail.com>
2024-06-26 09:21:31 +0300

    change quest subscription

--- src/widgets/quest-popover/ui/quest-popover.tsx
+++ src/widgets/quest-popover/ui/quest-popover.tsx
@@ -4,10 +4,15 @@ import { Cross, Telegram, Ticket } from "@/assets/icons";
 import { Button } from "@/shared/ui/button";
 import { Popover, usePopoverContext } from "@/shared/ui/popover";
 import { formatNumber } from "@/shared/lib/helpers/format-number";
+import { TelegramClient } from "@/shared/api/types";
 
 import styles from "./index.module.css";
 
 export const QuestPopover = () => {
+    const tg = (
+        window as Window & typeof globalThis & { Telegram: TelegramClient }
+    ).Telegram.WebApp;
+
     const { data: quest } = usePopoverContext() as { data: Quest };
     const [completeQuest] = useCompleteQuestMutation();
 
@@ -17,6 +22,7 @@ export const QuestPopover = () => {
         if (quest === undefined || !quest) return;
 
         try {
+            tg.openLink(quest?.url);
             await completeQuest({ id: quest.id }).unwrap();
         } catch (error) {
             console.error(error);
@@ -31,7 +37,7 @@ export const QuestPopover = () => {
                 </span>
                 <hgroup>
                     <h2 className="text-green text-center text-shadow-green font-secondary">
-                        Join our telegram channel
+                        {quest?.description}
                     </h2>
                     <p>
                         +{formatNumber(quest?.amount || 0)}
@@ -41,17 +47,18 @@ export const QuestPopover = () => {
             </header>
 
             <Button
+                onClick={onClickHandler}
                 className={`${styles["subscribe-button"]} button-blue text-shadow-blue font-secondary`}
             >
                 Subscribe
             </Button>
 
-            <Button
+            {/* <Button
                 onClick={onClickHandler}
                 className={`${styles["subscribe-button"]} button-blue text-shadow-blue font-secondary`}
             >
                 Verify
-            </Button>
+            </Button> */}
 
             <Popover.Close className={styles["close-button"]}>
                 <Cross className="text-green" />
