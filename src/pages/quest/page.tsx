f5c932dfe44168616f784cb66fb2ec836f4fed38
Cashisfsg <cashisfsg@gmail.com>
2024-06-26 16:29:32 +0300

    test new condition

--- src/pages/quest/page.tsx
+++ src/pages/quest/page.tsx
@@ -45,7 +45,7 @@ export const QuestPage = () => {
                             <>
                                 {/* {quests.length !== 0 ? ( */}
                                 <DynamicList
-                                    hasNextPage={page * limit === quests.length}
+                                    hasNextPage={page * limit < quests.length}
                                     isNextPageLoading={isFetching}
                                     isSuccess={isSuccess}
                                     items={quests}
