8db983fa26ef7142c3d59dd9a4434c328929a825
Cashisfsg <cashisfsg@gmail.com>
2024-06-29 21:27:15 +0300

    add qr code

--- src/pages/redirect/page.tsx
+++ src/pages/redirect/page.tsx
@@ -62,7 +62,7 @@ export const RedirectPage = () => {
                 navigate("/root/app/welcome");
                 return;
             } catch (error) {
-                throw new Error(error);
+                throw new Error(error?.message);
             }
         })();
     }, []);
