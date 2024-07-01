49a44f05c75538799c9f6bc70855b86dc1187cbb
Cashisfsg <cashisfsg@gmail.com>
2024-07-02 00:39:56 +0300

    back to video. add playIsinline

--- src/pages/main-layout.tsx
+++ src/pages/main-layout.tsx
@@ -1,24 +1,27 @@
 import { Outlet } from "react-router-dom";
 
-import Cats from "@/assets/img/cats.gif";
+import Cats from "@/assets/video/cats.webm";
 
 export const MainLayout = () => {
     return (
         <>
-            {/* <video
+            <video
                 autoPlay
                 muted
                 loop
+                preload="auto"
+                controls={false}
+                playsInline
                 className="background-video content-wrapper"
             >
                 <source src={Cats} type="video/webm" />
                 Your browser does not support the video tag.
-            </video> */}
-            <img
+            </video>
+            {/* <img
                 src={Cats}
                 alt="Background animation"
                 className="background-video content-wrapper"
-            />
+            /> */}
 
             <Outlet />
         </>
